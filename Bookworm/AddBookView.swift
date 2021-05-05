d022fc1e08d3db9809042bbc4282296c63db3a72
Olivier Van hamme <nicefiction@gmail.com>
2021-05-05 07:45:32 +0200

    Creates books with Core Data .

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,100 @@
+// MARK: AddBookView.swift
+
+import SwiftUI
+
+
+
+struct AddBookView: View {
+    
+     // ////////////////////////
+    //  MARK: PROPERTY WRAPPERS
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var author: String = ""
+    @State private var title: String = ""
+    @State private var genre: String = ""
+    @State private var rating: Int = 3
+    @State private var review: String = ""
+    // @State private var genreIndex: Int = 0
+    
+    
+    
+     // /////////////////
+    //  MARK: PROPERTIES
+    
+    let genres: [String] = [
+        "Adventure" , "Thriller" , "Fantasy" , "Horror" , "Kids" , "Mystery" , "Poetry" , "Romance"
+    ]
+    
+    
+
+     // /////////////////////////
+    // MARK: COMPUTED PROPERTIES
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of Book :" , text : $title)
+                    TextField("Name of Author :" , text : $author)
+                    Picker("Genre" , selection: $genre) {
+                        ForEach(genres , id : \.self) { (genre: String) in
+                            Text(genre)
+                        }
+                    }
+                }
+                Section {
+                    Picker("Rating" , selection : $rating) {
+                        ForEach(0..<6) {
+                            Text("\($0)")
+                        }
+                    }
+                    TextField("Write a review :" , text : $review)
+                }
+                HStack {
+                    Spacer()
+                    Button("Save Book") {
+                        /**
+                         Creates an instance of the `Book class`
+                         using our `managed object context` ,
+                         */
+                        let newBook = Book(context : managedObjectContext)
+                        /**
+                         Copies in all the values from our form ,
+                         */
+                        newBook.author = self.author
+                        newBook.title = self.title
+                        newBook.genre = self.genre
+                        newBook.rating = Int16(self.rating)
+                        newBook.review = self.review
+                        /**
+                         Saves the managed object context .
+                         */
+                        try? self.managedObjectContext.save()
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    Spacer()
+                }
+            }
+            .navigationBarTitle(Text("Add a Book"))
+        }
+    }
+}
+
+
+
+
+
+ // ///////////////
+//  MARK: PREVIEWS
+
+struct AddBookView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        AddBookView()
+    }
+}
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -1,80 +1,80 @@
-//
-//  ContentView.swift
-//  Bookworm
-//
-//  Created by Olivier Van hamme on 05/05/2021.
-//
+// MARK: ContentView.swift
 
 import SwiftUI
 import CoreData
 
+
+
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    
+     // ////////////////////////
+    //  MARK: PROPERTY WRAPPERS
+    /**
+     When we place an object into the environment for a view ,
+     it becomes accessible to that view and any views that can call it an ancestor .
+     So, if we have View A that contains inside it View B ,
+     anything in the environment for View A
+     will also be in the environment for View B .
+     Taking this a step further ,
+     if View A happens to be a NavigationView ,
+     any views that are pushed onto the navigation stack
+     have that NavigationView as their ancestor
+     so they share the same environment .
+     Now think about sheets —those are full-screen pop up windows on iOS .
+     Yes , one screen might have caused them to appear ,
+     but does that mean the presented view can call the original its ancestor ?
+     SwiftUI has an answe r, and it is “no” ,
+     which means that when we present a new view as a sheet
+     we need to explicitly pass in a managed object context for it to use .
+     As the new AddBookView will be shown as a sheet from ContentView ,
+     we need to add a managed object context property to ContentView
+     so it can be passed in .
+     */
+    @Environment(\.managedObjectContext) var managedObjectModel
+    /**
+     A fetch request reading all the books we have ( so we can test everything worked ) :
+     */
+    @FetchRequest(entity : Book.entity() ,
+                  sortDescriptors : []) var books: FetchedResults<Book>
+    @State private var isShowingSheet: Bool = false
+    
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+     // //////////////////////////
+    //  MARK: COMPUTED PROPERTIES
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+        
+        NavigationView {
+            Text("Count : \(books.count)")
+                .navigationBarTitle(Text("Bookworm"))
+                .navigationBarItems(trailing : Button(action : {
+                    isShowingSheet.toggle()
+                } , label : {
+                    Image(systemName: "plus.circle")
+                        .font(.title)
+                }))
+                .sheet(isPresented: $isShowingSheet) {
+                    AddBookView().environment(\.managedObjectContext ,
+                                              self.managedObjectModel)
+                }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+
+
+
+
+ // ///////////////
+//  MARK: PREVIEWS
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        
+//        ContentView().environment(\.managedObjectContext ,
+//                                  PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
